01363438ef7ef9e9f280dea621ec6f588d01b76c
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2017-09-07 00:40:57 +0100

    first commit

---
+++ sniff-probe-req.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from scapy.all import *
+from os import geteuid
+from sys import argv, exit
+from argparse import ArgumentParser
+
+if __name__ == "__main__":
+    ap = ArgumentParser(description="Sniff Wifi probe requests")
+    ap.add_argument("-i", "--interface", required=True, help="network interface to use")
+    args = vars(ap.parse_args())
+
+    if not geteuid() == 0:
+        exit("[!] You must be root")
+
+    sniff(iface=args["interface"], filter="type mgt subtype probe-req", prn=lambda x: x.sprintf("{Dot11ProbeReq:%RadioTap.addr2%\t%Dot11Elt.info%}"))
