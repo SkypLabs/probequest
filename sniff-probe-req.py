e069a2292be4f38b6de305bf8290894e69ebf386
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2017-09-10 17:53:28 +0100

    reorder parameters alphabetically

--- sniff-probe-req.py
+++ sniff-probe-req.py
@@ -27,16 +27,15 @@ def parseProbeReq(packet):
             outfile.writerow([timestamp, s_mac, essid])
 
 if __name__ == "__main__":
-    ap = ArgumentParser(description="Wi-Fi probe requests sniffer")
+    ap = ArgumentParser(description="Wi-Fi Probe Requests Sniffer")
+    essid_arguments = ap.add_mutually_exclusive_group()
 
+    essid_arguments.add_argument("-e", "--essid", nargs="+", help="ESSID of the APs to filter (space-separated list)")
     ap.add_argument("--exclude", nargs="+", help="MAC addresses of the stations to exclude (space-separated list)")
     ap.add_argument("-i", "--interface", required=True, help="wireless interface to use (must be in monitor mode)")
     ap.add_argument("-o", "--output", type=FileType("a", encoding="UTF-8"), help="output file to save the captured data (CSV format)")
-    ap.add_argument("-s", "--station", nargs="+", help="MAC addresses of the stations to filter (space-separated list)")
-
-    essid_arguments = ap.add_mutually_exclusive_group()
-    essid_arguments.add_argument("-e", "--essid", nargs="+", help="ESSID of the APs to filter (space-separated list)")
     essid_arguments.add_argument("-r", "--regex", help="regex to filter the ESSIDs")
+    ap.add_argument("-s", "--station", nargs="+", help="MAC addresses of the stations to filter (space-separated list)")
 
     args = vars(ap.parse_args())
 
