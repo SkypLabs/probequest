cd404c5d7ad19f144f4cb33fec2532db2e01ede1
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2017-09-10 18:13:03 +0100

    add 'setup.py'

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from os.path import dirname, abspath, join
+from codecs import open
+
+DIR = dirname(abspath(__file__))
+VERSION = '0.3.0'
+
+with open(join(DIR, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'sniff-probe-req',
+    version = VERSION,
+    description = 'Wi-Fi Probe Requests Sniffer',
+    long_description = long_description,
+    license = 'GPLv3',
+    keywords = 'wifi wireless security sniffer',
+    author = 'Paul-Emmanuel Raoul',
+    author_email = 'skyper@skyplabs.net',
+    url = 'https://github.com/SkypLabs/sniff-probe-req',
+    download_url = 'https://github.com/SkypLabs/sniff-probe-req/archive/v{0}.zip'.format(VERSION),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    scripts = ['sniff-probe-req.py'],
+    install_requires = ['argparse', 'scapy-python3'],
+)
