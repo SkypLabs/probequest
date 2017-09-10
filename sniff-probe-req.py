377c47a9f3c319501dacb7916dbdc755f8b50e56
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2017-09-10 17:05:25 +0100

    rename one argument

--- sniff-probe-req.py
+++ sniff-probe-req.py
@@ -25,16 +25,16 @@ if __name__ == "__main__":
     ap = ArgumentParser(description="Wi-Fi probe requests sniffer")
     ap.add_argument("-e", "--essid", nargs="+", help="ESSID of the APs to filter (space-separated list)")
     ap.add_argument("--exclude", nargs="+", help="MAC addresses of the stations to exclude (space-separated list)")
-    ap.add_argument("-f", "--file", type=FileType("a", encoding="UTF-8"), help="output file to save the captured data (CSV format)")
     ap.add_argument("-i", "--interface", required=True, help="wireless interface to use (must be in monitor mode)")
+    ap.add_argument("-o", "--output", type=FileType("a", encoding="UTF-8"), help="output file to save the captured data (CSV format)")
     ap.add_argument("-s", "--station", nargs="+", help="MAC addresses of the stations to filter (space-separated list)")
     args = vars(ap.parse_args())
 
     if not geteuid() == 0:
         exit("[!] You must be root")
 
-    if args["file"]:
-        outfile = writer(args["file"], delimiter=";")
+    if args["output"]:
+        outfile = writer(args["output"], delimiter=";")
 
     if args["essid"]:
         essid_filter = args["essid"]
