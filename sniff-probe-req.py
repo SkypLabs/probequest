b67017cc642c1b481ffa8681237ce9647d2fb0e3
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2017-09-10 14:03:44 +0100

    add essid filtering

--- sniff-probe-req.py
+++ sniff-probe-req.py
@@ -13,6 +13,9 @@ def parseProbeReq(packet):
     essid = packet.getlayer(Dot11ProbeReq).info.decode("utf-8")
 
     if essid:
+        if "essid_filter" in globals() and not essid in essid_filter:
+            return
+
         print("{timestamp} - {s_mac} -> {essid}".format(timestamp=timestamp, s_mac=s_mac, essid=essid))
 
         if "outfile" in globals():
@@ -20,6 +23,7 @@ def parseProbeReq(packet):
 
 if __name__ == "__main__":
     ap = ArgumentParser(description="Wi-Fi probe requests sniffer")
+    ap.add_argument("-e", "--essid", nargs="+", help="ESSID of the APs to filter (space-separated list)")
     ap.add_argument("-f", "--file", type=FileType("a", encoding="UTF-8"), help="output file to save the captured data (CSV format)")
     ap.add_argument("-i", "--interface", required=True, help="wireless interface to use (must be in monitor mode)")
     ap.add_argument("-s", "--stations", nargs="+", help="MAC addresses of the stations to filter (space-separated list)")
@@ -30,6 +34,9 @@ if __name__ == "__main__":
 
     if args["file"]:
         outfile = writer(args["file"], delimiter=";")
+
+    if args["essid"]:
+        essid_filter = args["essid"]
 
     filter = "type mgt subtype probe-req"
 
