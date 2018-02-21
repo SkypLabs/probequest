ac5d491d92bcd9865cdae480681032d7daf3d565
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2018-02-21 17:55:12 +0000

    update Sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -427,3 +427,13 @@ epub_exclude_files = ['search.html']
 # If false, no index is generated.
 #
 # epub_use_index = True
+
+# -- Options for GitHub integration ----------------------------------------------
+
+html_context = {
+    'display_github': True, # Integrate GitHub
+    'github_user': 'SkypLabs', # Username
+    'github_repo': 'sniff-probe-req', # Repo name
+    'github_version': 'master', # Version
+    'conf_py_path': '/docs/', # Path in the checkout to the docs root
+}
