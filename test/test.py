0dd6e79119259436888b5ebf2a9f06dd46f4a9e0
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2019-01-27 18:57:53 +0000

    fix linting errors in the unit tests

--- test/test.py
+++ test/test.py
@@ -1,23 +1,54 @@
-import pylint.lint
+"""
+Unit tests written with the 'unittest' module.
+"""
+
 import unittest
-from datetime import datetime
+import pylint.lint
+from scapy.layers.dot11 import RadioTap, Dot11, Dot11Elt, Dot11ProbeReq
+from scapy.packet import fuzz
 from netaddr.core import AddrFormatError
 from probequest.probe_request import ProbeRequest
 from probequest.probe_request_sniffer import ProbeRequestSniffer
-from scapy.all import *
+
+# pylint: disable=unused-variable
 
 class TestProbeRequest(unittest.TestCase):
+    """
+    Unit tests for the ProbeRequest class.
+    """
+
     def test_without_parameters(self):
+        """
+        Initialises a ProbeRequest object
+        without any parameter.
+        """
+
+        # pylint: disable=no-value-for-parameter
+
         with self.assertRaises(TypeError):
             probe_req = ProbeRequest()
 
     def test_with_only_one_parameter(self):
+        """
+        Initialises a ProbeRequest object
+        with only one parameter.
+        """
+
+        # pylint: disable=no-value-for-parameter
+
         timestamp = 1517872027.0
 
         with self.assertRaises(TypeError):
             probe_req = ProbeRequest(timestamp)
 
     def test_with_only_two_parameters(self):
+        """
+        Initialises a ProbeRequest object
+        with only two parameters.
+        """
+
+        # pylint: disable=no-value-for-parameter
+
         timestamp = 1517872027.0
         s_mac = "aa:bb:cc:dd:ee:ff"
 
@@ -25,6 +56,13 @@ class TestProbeRequest(unittest.TestCase):
             probe_req = ProbeRequest(timestamp, s_mac)
 
     def test_create_a_probe_request(self):
+        """
+        Creates a new ProbeRequest with all
+        the required parameters.
+        """
+
+        # pylint: disable=no-self-use
+
         timestamp = 1517872027.0
         s_mac = "aa:bb:cc:dd:ee:ff"
         essid = "Test ESSID"
@@ -32,6 +70,11 @@ class TestProbeRequest(unittest.TestCase):
         probe_req = ProbeRequest(timestamp, s_mac, essid)
 
     def test_bad_mac_address(self):
+        """
+        Initialises a ProbeRequest object
+        with a malformed MAC address.
+        """
+
         timestamp = 1517872027.0
         s_mac = "aa:bb:cc:dd:ee"
         essid = "Test ESSID"
@@ -40,6 +83,11 @@ class TestProbeRequest(unittest.TestCase):
             probe_req = ProbeRequest(timestamp, s_mac, essid)
 
     def test_print_a_probe_request(self):
+        """
+        Initialises a ProbeRequest object
+        and prints it.
+        """
+
         timestamp = 1517872027.0
         s_mac = "aa:bb:cc:dd:ee:ff"
         essid = "Test ESSID"
@@ -50,27 +98,74 @@ class TestProbeRequest(unittest.TestCase):
         self.assertNotEqual(str(probe_req).find("aa:bb:cc:dd:ee:ff (None) -> Test ESSID"), -1)
 
 class TestProbeRequestSniffer(unittest.TestCase):
+    """
+    Unit tests for the ProbeRequestSniffer class.
+    """
+
     def test_without_parameters(self):
+        """
+        Initialises a ProbeRequestSniffer object
+        without parameters.
+        """
+
+        # pylint: disable=no-value-for-parameter
+
         with self.assertRaises(TypeError):
             sniffer = ProbeRequestSniffer()
 
     def test_bad_display_function(self):
+        """
+        Initialises a ProbeRequestSniffer object
+        with a non-callable display function.
+        """
+
         with self.assertRaises(TypeError):
             sniffer = ProbeRequestSniffer("wlan0", display_func="Test")
 
     def test_bad_storage_function(self):
+        """
+        Initialises a ProbeRequestSniffer object
+        with a non-callable storage function.
+        """
+
         with self.assertRaises(TypeError):
             sniffer = ProbeRequestSniffer("wlan0", storage_func="Test")
 
     def test_create_sniffer(self):
+        """
+        Creates a ProbeRequestSniffer object.
+        """
+
+        # pylint: disable=no-self-use
+
         sniffer = ProbeRequestSniffer("wlan0")
 
     def test_stop_before_start(self):
+        """
+        Creates a ProbeRequestSniffer object
+        and stops the sniffer before starting
+        it.
+        """
+
+        # pylint: disable=no-self-use
+
         sniffer = ProbeRequestSniffer("wlan0")
         sniffer.stop()
 
 class TestProbeRequestParser(unittest.TestCase):
+    """
+    Unit tests for the ProbeRequestParser class.
+    """
+
     def test_no_probe_request_layer(self):
+        """
+        Creates a non-probe-request Wi-Fi
+        packet and parses it with the
+        'ProbeRequestParser.parse()' function.
+        """
+
+        # pylint: disable=no-self-use
+
         packet = RadioTap() \
             / Dot11(
                 addr1="ff:ff:ff:ff:ff:ff",
@@ -81,6 +176,15 @@ class TestProbeRequestParser(unittest.TestCase):
         ProbeRequestSniffer.ProbeRequestParser.parse(packet)
 
     def test_empty_essid(self):
+        """
+        Creates a probe request packet
+        with an empty ESSID field and
+        parses it with the
+        'ProbeRequestParser.parse()' function.
+        """
+
+        # pylint: disable=no-self-use
+
         packet = RadioTap() \
             / Dot11(
                 addr1="ff:ff:ff:ff:ff:ff",
@@ -95,12 +199,30 @@ class TestProbeRequestParser(unittest.TestCase):
         ProbeRequestSniffer.ProbeRequestParser.parse(packet)
 
     def test_fuzz_packets(self):
+        """
+        Parses 1000 randomly-generated probe
+        requests with the ProbeRequestParser.parse()
+        function.
+        """
+
+        # pylint: disable=no-self-use
+
         for i in range(0, 1000):
             packet = RadioTap()/fuzz(Dot11()/Dot11ProbeReq()/Dot11Elt())
             ProbeRequestSniffer.ProbeRequestParser.parse(packet)
 
 class TestLinter(unittest.TestCase):
-    def test_linter(self):
+    """
+    Unit tests for Python linters.
+    """
+
+    def test_pylint(self):
+        """
+        Executes Pylint.
+        """
+
+        # pylint: disable=no-self-use
+
         pylint.lint.Run([
             "probequest",
             "test"
