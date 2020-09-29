8e1a4b8888628b98156b8701f26406f2603b7183
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2020-09-29 15:57:12 +0100

    Use the logging package in 'probequest.cli'

--- probequest/__init__.py
+++ probequest/__init__.py
@@ -2,6 +2,20 @@
 ProbeQuest package.
 """
 
+import logging
 from pkg_resources import get_distribution
 
 __version__ = get_distribution("probequest").version
+
+
+def set_up_package_logger():
+    """
+    Sets up the package logger.
+    """
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.NullHandler())
+
+
+set_up_package_logger()
