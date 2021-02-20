72322fd79ab268b838bb2eb2ab695a377b04d2d1
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2021-02-20 02:02:28 +0000

    Refactor 'ProbeRequestParser'

--- probequest/probe_request_parser.py
+++ probequest/probe_request_parser.py
@@ -2,75 +2,33 @@
 Probe request parser module.
 """
 
-import logging
-from queue import Empty
-from re import match
-from threading import Thread, Event
-
+from scapy.pipetool import Drain
 from scapy.layers.dot11 import RadioTap, Dot11ProbeReq
 
-from .probe_request import ProbeRequest
+from probequest.probe_request import ProbeRequest
 
 
-class ProbeRequestParser(Thread):
+class ProbeRequestParser(Drain):
     """
-    A Wi-Fi probe request parsing thread.
+    A Wi-Fi probe request parsing drain.
     """
 
-    def __init__(self, config, new_packets):
-        super().__init__()
-
-        self.logger = logging.getLogger(__name__)
+    def __init__(self, config, name=None):
+        Drain.__init__(self, name=name)
 
         self.config = config
-        self.new_packets = new_packets
 
-        self.cregex = self.config.compiled_essid_regex
+    def push(self, msg):
+        try:
+            self._send(self.parse(msg))
+        except TypeError:
+            return
 
-        self.stop_parser = Event()
-
-    def run(self):
-        self.logger.debug("Starting the probe request parser")
-
-        # The parser continues to do its job even after the call of the
-        # join method if the queue is not empty.
-        while not self.stop_parser.isSet() or not self.new_packets.empty():
-            try:
-                packet = self.new_packets.get(timeout=1)
-                probe_request = self.parse(packet)
-
-                if probe_request is None:
-                    continue
-
-                if not probe_request.essid:
-                    continue
-
-                if (self.config.essid_filters is not None
-                        and probe_request.essid
-                        not in self.config.essid_filters):
-                    continue
-
-                if (self.cregex is not None
-                        and not
-                        match(self.cregex, probe_request.essid)):
-                    continue
-
-                self.config.display_func(probe_request)
-                self.config.storage_func(probe_request)
-
-                self.new_packets.task_done()
-            except Empty:
-                pass
-
-    def join(self, timeout=None):
-        """
-        Stops the probe request parsing thread.
-        """
-
-        self.logger.debug("Stopping the probe request parser")
-
-        self.stop_parser.set()
-        super().join(timeout)
+    def high_push(self, msg):
+        try:
+            self._high_send(self.parse(msg))
+        except TypeError:
+            return
 
     @staticmethod
     def parse(packet):
@@ -86,7 +44,8 @@ class ProbeRequestParser(Thread):
 
                 return ProbeRequest(timestamp, s_mac, essid)
 
-            return None
-        except UnicodeDecodeError:
+            # The packet is not a probe request.
+            raise TypeError
+        except UnicodeDecodeError as unicode_decode_err:
             # The ESSID is not a valid UTF-8 string.
-            return None
+            raise TypeError from unicode_decode_err
