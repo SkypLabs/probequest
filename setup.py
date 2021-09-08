578e8ca7f55d5405176b24e2815fa8bd0d521290
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2021-09-08 13:49:46 +0100

    Drop support for Python 3.5
    Python 3.5 has reached end-of-life in September 2020.

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
         "Topic :: System :: Networking",
         "Topic :: System :: Networking :: Monitoring",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
