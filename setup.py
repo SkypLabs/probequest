aa1d8eae8f724f1858e6034d47d1799aa3abedbd
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2021-09-08 13:37:04 +0100

    Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
     packages=find_packages(),
