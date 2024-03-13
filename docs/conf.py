8739ee106cd29e6cc4971401e14c66f82ad374d4
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2024-03-13 12:50:01 +0100

    Replace quotes with double quotes in Sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@ from probequest import __version__ as VERSION
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ProbeQuest'
-copyright = '2022, Paul-Emmanuel Raoul'
-author = 'Paul-Emmanuel Raoul'
+project = "ProbeQuest"
+copyright = "2022, Paul-Emmanuel Raoul"
+author = "Paul-Emmanuel Raoul"
 
 release = VERSION
 
@@ -20,33 +20,33 @@ release = VERSION
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-    'sphinxarg.ext',
-    'sphinxcontrib.seqdiag',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinxarg.ext",
+    "sphinxcontrib.seqdiag",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-master_doc = 'index'
+master_doc = "index"
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_context = {
-    'conf_py_path': '/docs/',
-    'display_github': True,
-    'github_user': 'SkypLabs',
-    'github_repo': 'probequest',
-    'github_version': 'develop',
-    "plausible_domain": 'probequest.skyplabs.net',
+    "conf_py_path": "/docs/",
+    "display_github": True,
+    "github_user": "SkypLabs",
+    "github_repo": "probequest",
+    "github_version": "develop",
+    "plausible_domain": "probequest.skyplabs.net",
 }
 
 # -- Extension configuration -------------------------------------------------
@@ -59,4 +59,4 @@ todo_include_todos = True
 # -- Options for sphinxcontrib-seqdiag extension -----------------------------
 # http://blockdiag.com/en/seqdiag/sphinxcontrib.html
 
-seqdiag_fontpath = '/usr/share/fonts/truetype/ipafont/ipagp.ttf'
+seqdiag_fontpath = "/usr/share/fonts/truetype/ipafont/ipagp.ttf"
