fcc52ff178288af886435a93ccc5d36bc2c0bb81
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2024-03-13 12:48:57 +0100

    Update comments in Sphinx config file

--- docs/conf.py
+++ docs/conf.py
@@ -1,39 +1,24 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # pylint: skip-file
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 from probequest import __version__ as VERSION
 
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'ProbeQuest'
 copyright = '2022, Paul-Emmanuel Raoul'
 author = 'Paul-Emmanuel Raoul'
 
-# The full version, including alpha/beta/rc tags
 release = VERSION
 
-
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
@@ -42,28 +27,17 @@ extensions = [
     'sphinxcontrib.seqdiag',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# The master toctree document.
 master_doc = 'index'
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 html_context = {
@@ -78,12 +52,11 @@ html_context = {
 # -- Extension configuration -------------------------------------------------
 
 # -- Options for todo extension ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html
 
-# If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
-
 # -- Options for sphinxcontrib-seqdiag extension -----------------------------
+# http://blockdiag.com/en/seqdiag/sphinxcontrib.html
 
-# Fontpath for seqdiag (truetype font).
 seqdiag_fontpath = '/usr/share/fonts/truetype/ipafont/ipagp.ttf'
