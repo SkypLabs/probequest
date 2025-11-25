39e159393977781170af0296fef5cb384746a00d
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2025-11-25 14:45:54 +0100

    Add device lookup logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
+use anyhow::{Context, Result, bail};
 use clap::Parser;
+use pcap::Device;
 
 mod cli;
 
 use cli::Cli;
 
-fn main() {
+fn main() -> Result<()> {
+    // Parse CLI arguments and options.
     let cli = Cli::parse();
+
+    // Determine network device to use.
+    let device: Device = if cli.interface.is_some() {
+        find_device_by_name(&cli.interface.unwrap())?
+    } else {
+        Device::lookup()
+            .context("Failed during network interface lookup")?
+            .context("No available device found via lookup")?
+    };
+
+    println!("Device: {}", device.name);
+
+    Ok(())
+}
+
+/// Checks if `name` is an available network device.
+fn find_device_by_name(name: &str) -> Result<Device> {
+    let devices = Device::list().context("Failed to list all available network devices")?;
+
+    for device in devices {
+        if device.name == name {
+            return Ok(device);
+        }
+    }
+
+    bail!("Device '{}' not found", name)
 }
